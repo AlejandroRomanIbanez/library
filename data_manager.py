dfe1f7e0c5cc5b662053c3dc97ea3256e551875f
Alejandro Roman Ibañez <roman.ibanez.alex@gmail.com>
2023-07-30 01:32:08 +0200

    Added Docstrings

--- data_manager.py
+++ data_manager.py
@@ -9,6 +9,16 @@ API_KEY = "Put Here Your API Key"
 
 
 def sort_books(sort_option, Book, Author):
+    """
+    Sort Books
+    Sorts the list of books based on the given sort_option.
+    Args:
+        sort_option (str): The sorting option in the format "field_order", e.g., "author_ASC".
+        Book (SQLAlchemy Model): The Book model class.
+        Author (SQLAlchemy Model): The Author model class.
+    Returns:
+        list: A list of books sorted according to the sort_option.
+    """
     if sort_option:
         sort_by, sort_order = sort_option.split("_")
         if sort_by == "author":
@@ -31,6 +41,16 @@ def sort_books(sort_option, Book, Author):
 
 
 def search_books(search_query, Book, Author):
+    """
+    Search Books
+    Searches for books or authors that match the given search_query.
+    Args:
+        search_query (str): The search query entered by the user.
+        Book (SQLAlchemy Model): The Book model class.
+        Author (SQLAlchemy Model): The Author model class.
+    Returns:
+        list: A list of books or authors that match the search_query.
+    """
     if search_query:
         return Book.query.join(Author).filter(
             or_(Book.title.like(f"%{search_query}%"), Author.name.like(f"%{search_query}%"))
@@ -41,6 +61,15 @@ def search_books(search_query, Book, Author):
 
 
 def get_isbn(title):
+    """
+    Get ISBN
+    Retrieves the ISBN (International Standard Book Number) for the given book title
+    using the Google Books API.
+    Args:
+        title (str): The title of the book.
+    Returns:
+        str or None: The ISBN of the book if found, or None if not found.
+    """
     google_books_api_url = f"https://www.googleapis.com/books/v1/volumes?q=intitle:{title}"
     response = requests.get(google_books_api_url)
     data = response.json()
@@ -54,6 +83,15 @@ def get_isbn(title):
 
 
 def get_book_cover_url(isbn):
+    """
+    Get Book Cover URL
+    Retrieves the URL for the book cover image based on the given ISBN
+    using the Google Books API.
+    Args:
+        isbn (str): The ISBN of the book.
+    Returns:
+        str or None: The URL of the book cover image if found, or None if not found.
+    """
     google_books_api_url = f"https://www.googleapis.com/books/v1/volumes?q=isbn:{isbn}"
     response = requests.get(google_books_api_url)
     data = response.json()
@@ -65,6 +103,15 @@ def get_book_cover_url(isbn):
 
 
 def get_book_description(book_title):
+    """
+    Get Book Description
+    Retrieves the description of the book based on the given book title
+    using the Google Books API.
+    Args:
+        book_title (str): The title of the book.
+    Returns:
+        str: The description of the book if found, or a default message if not found.
+    """
     url = f'https://www.googleapis.com/books/v1/volumes?q=intitle:{book_title}&key={API_KEY}'
     response = requests.get(url)
     if response.status_code == 200:
@@ -80,6 +127,14 @@ def get_book_description(book_title):
 
 
 def extract_year_from_date(date_string):
+    """
+    Extract Year from Date
+    Extracts the year from a given date string using regular expressions.
+    Args:
+        date_string (str): The date string in various formats.
+    Returns:
+        int or None: The extracted year as an integer if found, or None if not found.
+    """
     formats_to_try = ["%Y-%m-%d", "%Y-%m", "%Y"]
     for date_format in formats_to_try:
         match = re.match(r"\d{4}", date_string)
@@ -88,6 +143,15 @@ def extract_year_from_date(date_string):
     return None
 
 def get_book_year_by_title(book_title):
+    """
+    Get Book Year by Title
+    Retrieves the publication year of the book based on the given book title
+    using the Google Books API.
+    Args:
+        book_title (str): The title of the book.
+    Returns:
+        int or str: The publication year of the book if found, or a default message if not found.
+    """
     google_books_api_url = f"https://www.googleapis.com/books/v1/volumes?q=intitle:{book_title}&key={API_KEY}"
     response = requests.get(google_books_api_url)
     if response.status_code == 200:
@@ -104,6 +168,15 @@ def get_book_year_by_title(book_title):
 
 
 def get_author_info_by_name(author_name):
+    """
+    Get Author Info by Name
+    Retrieves information about an author based on their name from Wikipedia.
+    Args:
+        author_name (str): The name of the author.
+    Returns:
+        dict or None: A dictionary containing information about the author
+                      (name, description, and image URL) if found, or None if not found.
+    """
     wiki_wiki = Wikipedia(user_agent='lybrary (put here your email user)')
     page = wiki_wiki.page(author_name)
 
--- data_models.py
+++ data_models.py
@@ -6,6 +6,19 @@ from data_manager import get_book_cover_url
 db = SQLAlchemy()
 
 class Author(db.Model):
+  """
+  Author Model
+  Represents an author in the database.
+  Attributes:
+  id (int): Primary key for the author.
+  name (str): The name of the author.
+  birth_date (Date): The birth date of the author.
+  death_date (Date): The death date of the author.
+  book_id (Relationship): One-to-many relationship with Book model, representing the books written by the author.
+  Methods:
+  __repr__(): Returns a string representation of the Author object.
+  __str__(): Returns a human-readable string representation of the Author object.
+    """
   id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String, nullable=False)
   birth_date = db.Column(db.Date, nullable=True)
@@ -21,6 +34,22 @@ class Author(db.Model):
 
 
 class Book(db.Model):
+  """
+  Book Model
+  Represents a book in the database.
+  Attributes:
+  id (int): Primary key for the book.
+  isbn (int): The ISBN (International Standard Book Number) of the book.
+  title (str): The title of the book.
+  rating (float): The rating of the book (0.0 to 10.0).
+  publication_year (int): The publication year of the book.
+  author_id (int): Foreign key referencing the Author model, representing the author of the book.
+  Properties:
+  cover_url (str): Property that returns the cover URL of the book.
+  Methods:
+  __repr__(): Returns a string representation of the Book object.
+  __str__(): Returns a human-readable string representation of the Book object.
+  """
   id = db.Column(db.Integer, primary_key=True)
   isbn = db.Column(db.Integer)
   title = db.Column(db.String, nullable=False)
@@ -32,6 +61,12 @@ class Book(db.Model):
   
   @property
   def cover_url(self):
+      """
+      Cover URL Property
+      Retrieves the cover URL of the book using the get_book_cover_url function.
+      Returns:
+          str: The URL of the book cover image.
+      """
       return get_book_cover_url(self.isbn)
 
   def __repr__(self):
@@ -42,11 +77,21 @@ class Book(db.Model):
     return f"title={self.title}, publication_year={self.publication_year}"
 
 def create_tables(app):
+    """
+    Create Tables
+    Creates the database tables based on the defined models.
+    Args:
+        app (Flask): The Flask application instance.
+    """
     with app.app_context():
         db.create_all()
 
 
 def print_all_data():
+    """
+    Print All Data
+    Retrieves all authors and books from the database and prints their information.
+    """
     all_authors = Author.query.all()
     all_books = Book.query.all()
 
